39e2e3e2b28512a4ca4665b1ed70540b1f7a734f
Akrifstg <akrifisofficial@gmail.com>
2025-08-16 18:06:30 +0600

    Create anime.js

---
+++ data/anime.js
@@ -0,0 +1,19 @@
+[
+    {
+        "id": "1",
+        "title": "Восхождение героя щита 4",
+        "cover": "https://shikimori.one/uploads/poster/animes/57907/0a6bbba4f57b7d643782190e8e7b7e82.jpeg",
+        "episodes": [
+            {
+                "number": 1,
+                "title": "скоро",
+                "vk_url": "oid=-231399388_456239023"
+            },
+            {
+                "number": 2,
+                "title": "скоро",
+                "vk_url": "oid=-231399388_456239039"
+            }
+        ]
+    },
+]
